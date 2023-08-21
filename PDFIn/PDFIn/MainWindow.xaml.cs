448912b639758d9e00da85b2efb23d5596ebbcb2
liuaoran <liuaoran@kdanmobile.com>
2023-08-21 19:47:30 +0800

    Create program& mupdfcore pkg

---
+++ PDFIn/PDFIn/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using MuPDFCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PDFIn
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            using MuPDFContext muPDFContext = new MuPDFContext();//Open a PDF document
+            using MuPDFDocument document = new MuPDFDocument(muPDFContext, "path/to/document.pdf");
+        }
+    }
+}
