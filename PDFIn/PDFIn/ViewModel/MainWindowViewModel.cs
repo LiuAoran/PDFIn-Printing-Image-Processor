cdd42cff7d32f241c171e0018f00cb28a479411f
liuaoran <liuaoran@kdanmobile.com>
2023-08-24 20:13:33 +0800

    password

--- PDFIn/PDFIn/ViewModel/MainWindowViewModel.cs
+++ PDFIn/PDFIn/ViewModel/MainWindowViewModel.cs
@@ -7,14 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PDFIn.Messenger;
-using MuPDFCore;
+using iText.Kernel.Pdf.Colorspace;
+using iText.Kernel.Pdf;
 
 namespace PDFIn.ViewModel
 {
     public partial class MainWindowViewModel : ObservableRecipient
     {
         private string filePath = string.Empty;
-        private MuPDFDocument? document = null;
+        private PdfDocument document;
+
         public MainWindowViewModel()
         {
             RegistMessenger();
@@ -30,14 +32,7 @@ namespace PDFIn.ViewModel
 
         private void InitPDFDocument(string filePath)
         {
-            using MuPDFContext ctx = new MuPDFContext();
-            using MuPDFDocument document = new MuPDFDocument(ctx, filePath);
-
-            int pageIndex = 2;
-
-            double zoomLevel = 6;
-
-            document.SaveImage(pageIndex, zoomLevel, PixelFormats.RGBA, "output.png", RasterOutputFileTypes.PNG);
+            document = new PdfDocument(new PdfWriter(filePath));
         }
     }
 }
